e285d33fa8429a2cef0b2f388e494a9505fc6232
chris0512 <kyang3200@gmail.com>
2022-08-05 22:38:02 +0800

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Yang Chi on 2022/8/5.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
